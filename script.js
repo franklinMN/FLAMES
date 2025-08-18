fc1163ca13c3ba4860b8e41155f4ad16fc9af6a9
franklinMN <frankmnwinter@gmail.com.>
2025-08-18 17:16:19 +0530

    moved logic to the frontend

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { calculateFlames } from "./logic.js";
+
 const f_head = document.getElementById("f-header");
 const f_main = document.getElementById("f-main");
 const f_result = document.getElementById("f-result");
@@ -5,52 +7,30 @@ const f_result = document.getElementById("f-result");
 const input1 = document.getElementById("person1");
 const input2 = document.getElementById("person2");
 
-async function getInputs() {
-  let payload;
+function getInputs() {
+  let person1, person2;
 
   if (input1.value.trim() != "" && input2.value.trim() != "") {
-    payload = {
-      person1: input1.value,
-      person2: input2.value,
-    };
+    person1 = input1.value;
+    person2 = input2.value;
   } else {
     alert("Empty inputs, please enter the names.");
     return;
   }
 
-  try {
-    const response = await fetch(
-      "https://flames-backend-api.onrender.com/flames",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(payload),
-      }
-    );
+  const res = calculateFlames(person1, person2);
 
-    if (!response.ok) {
-      throw new Error("Failed to send data");
-    }
+  document.querySelector("#f-result p").textContent = res;
 
-    const res = await response.json();
-    console.log(res.result);
-    document.querySelector("#f-result p").textContent = res.result;
+  f_result.classList.remove("hide");
+  f_head.classList.add("hide");
+  f_main.classList.add("hide");
 
-    f_result.classList.remove("hide");
-    f_head.classList.add("hide");
-    f_main.classList.add("hide");
-
-    requestAnimationFrame(() => {
-      f_result.classList.add("visible");
-      f_head.classList.remove("visible");
-      f_main.classList.remove("visible");
-    });
-  } catch (error) {
-    console.error("Error:", error);
-    alert("Failed to load data, refresh and try again!");
-  }
+  requestAnimationFrame(() => {
+    f_result.classList.add("visible");
+    f_head.classList.remove("visible");
+    f_main.classList.remove("visible");
+  });
 }
 
 //submit
